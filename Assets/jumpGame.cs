a3a60236dd2981598deac1bc10b24bf543dd886e
fade <1039661493@qq.com>
2021-06-18 17:08:20 +0800

    commit_test_xc

---
+++ Assets/jumpGame.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jumpGame : MonoBehaviour
+{
+    public  enum gameStates{stop,beginToEnd,EndToBegin}; 
+    public gameStates gameState= gameStates.stop;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
