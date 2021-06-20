91767fde662195263f4cd278e08db85251259b34
HXC12 <1039661493@qq.com>
2021-06-20 17:50:12 +0800

    添加了建晖的小黑人和testgit

--- Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
+++ Assets/StarterAssets/InputSystem/StarterAssetsInputs.cs
@@ -1,89 +1,127 @@
 using UnityEngine;
+
 #if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
+
 using UnityEngine.InputSystem;
+
 #endif
 
 namespace StarterAssets
 {
-	public class StarterAssetsInputs : MonoBehaviour
-	{
-		[Header("Character Input Values")]
-		public Vector2 move;
-		public Vector2 look;
-		public bool jump;
-		public bool sprint;
+    public class StarterAssetsInputs : MonoBehaviour
+    {
+        private ThirdPersonController tpc;
+        private bool forwardJumping;
 
-		[Header("Movement Settings")]
-		public bool analogMovement;
+        [Header("Character Input Values")]
+        public Vector2 move;
+
+        public Vector2 look;
+        public bool jump;
+        public bool sprint;
+
+        [Header("Movement Settings")]
+        public bool analogMovement;
 
 #if !UNITY_IOS || !UNITY_ANDROID
-		[Header("Mouse Cursor Settings")]
-		public bool cursorLocked = true;
-		public bool cursorInputForLook = true;
+
+        [Header("Mouse Cursor Settings")]
+        public bool cursorLocked = true;
+
+        public bool cursorInputForLook = true;
 #endif
 
+        private void Start()
+        {
+            tpc = GetComponent<ThirdPersonController>();
+        }
+
 #if ENABLE_INPUT_SYSTEM && STARTER_ASSETS_PACKAGES_CHECKED
-		public void OnMove(InputValue value)
-		{
-			MoveInput(value.Get<Vector2>());
-		}
 
-		public void OnLook(InputValue value)
-		{
-			if(cursorInputForLook)
-			{
-				LookInput(value.Get<Vector2>());
-			}
-		}
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            MoveInput(context.ReadValue<Vector2>());
+        }
 
-		public void OnJump(InputValue value)
-		{
-			JumpInput(value.isPressed);
-		}
+        public void OnLook(InputAction.CallbackContext context)
+        {
+            if (cursorInputForLook)
+            {
+                LookInput(context.ReadValue<Vector2>());
+            }
+        }
 
-		public void OnSprint(InputValue value)
-		{
-			SprintInput(value.isPressed);
-		}
+        public void OnJump(InputAction.CallbackContext context)
+        {
+            JumpInput(context.performed);
+        }
+
+        public void OnSprint(InputAction.CallbackContext context)
+        {
+            SprintInput(context.performed);
+        }
+
+        public void OnChangeJumpMode(InputAction.CallbackContext context)
+        {
+            if (context.performed)
+                tpc.ChangeJumpMode(context.action.actionMap.name);
+        }
+
+        public void OnForwardJump(InputAction.CallbackContext context)
+        {
+            if (context.started)
+            {
+                tpc.ForwardJumpStart();
+            }
+
+            if (context.canceled && forwardJumping)
+            {
+                tpc.ForwardJump(context.time - context.startTime);
+            }
+            else if (context.canceled)
+            {
+                tpc.ForwardJumpCancel();
+            }
+
+            forwardJumping = context.performed;
+        }
+
 #else
 	// old input sys if we do decide to have it (most likely wont)...
 #endif
 
+        public void MoveInput(Vector2 newMoveDirection)
+        {
+            move = newMoveDirection;
+        }
 
-		public void MoveInput(Vector2 newMoveDirection)
-		{
-			move = newMoveDirection;
-		} 
+        public void LookInput(Vector2 newLookDirection)
+        {
+            look = newLookDirection;
+        }
 
-		public void LookInput(Vector2 newLookDirection)
-		{
-			look = newLookDirection;
-		}
+        public void JumpInput(bool newJumpState)
+        {
+            jump = newJumpState;
+        }
 
-		public void JumpInput(bool newJumpState)
-		{
-			jump = newJumpState;
-		}
-
-		public void SprintInput(bool newSprintState)
-		{
-			sprint = newSprintState;
-		}
+        public void SprintInput(bool newSprintState)
+        {
+            sprint = newSprintState;
+        }
 
 #if !UNITY_IOS || !UNITY_ANDROID
 
-		private void OnApplicationFocus(bool hasFocus)
-		{
-			SetCursorState(cursorLocked);
-		}
+        private void OnApplicationFocus(bool hasFocus)
+        {
+            SetCursorState(cursorLocked);
+        }
 
-		private void SetCursorState(bool newState)
-		{
-			Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
-		}
+        private void SetCursorState(bool newState)
+        {
+            Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
+        }
 
 #endif
-
-	}
-	
+    }
 }
